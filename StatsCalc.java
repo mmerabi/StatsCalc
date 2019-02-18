1b4715046232f10d22793e9edb407ea80db02d23
mmerabi <merabimichael@gmail.com>
2019-02-17 16:22:10 -0800

    Update StatsCalc.java

--- StatsCalc.java
+++ StatsCalc.java
@@ -18,14 +18,16 @@ public class StatsCalc {
 
         //Asks user for the fraction and parses it into num and denom
 
+
         Scanner sc = new Scanner(in);
         String num_input = sc.next();
         StringTokenizer stok = new StringTokenizer(num_input, "/");
         num = Double.parseDouble(stok.nextToken());
         denom = Double.parseDouble(stok.nextToken());
-        out.println("You entered "+num+"/"+denom);
+        out.println("You entered "+num+"/"+denom+"\n");
 
 
+        //Catch statements for logical errors
         if(num == 0){
             out.println("There is a zero percent chance of this probability");
         }
@@ -34,13 +36,24 @@ public class StatsCalc {
         }
 
 
-            double odds = Double.valueOf(num)/Double.valueOf(denom);
-            int attempts = 0;
+        //Logical work of calculating the odds
+        double odds = Double.valueOf(num)/Double.valueOf(denom);
+        int attempts = 0;
 
-            do {attempts++;}
-            while  (Math.random() > odds);
-            System.out.println("attempts: "+attempts);
+        do {attempts++;}
+        while  (Math.random() > odds);
+
+        //Calculating the likelihood of user getting these results
+        double percent = Math.pow((1-odds),(attempts - 1));
+        percent = percent * odds;
+        percent = percent * 100;
 
 
-        }
+        //Printing out results
+        System.out.println("It took " + attempts +" attempts to hit your probability\n");
+        System.out.printf("This is  %.3f" , percent);
+        System.out.print("% likely to happen");
+
+
     }
+}
